a191464c5bbc322fe8040a8acde530c76dda0486
Ralph-Nuamah <boatengraphael3@gmail.com>
2024-05-16 10:39:26 +0000

    update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Twu3cMaO9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7qa883h15n2v3rj783mmj82f4u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
